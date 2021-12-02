9032bec516f703ae0b0329c2de049fd42e7b90aa
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-12-02 11:51:41 -0800

    anwer-03

--- tests/ali.c
+++ tests/ali.c
@@ -67,3 +67,26 @@ pid_t *fork_child()
 	}
 	return (*child_pid);
 }
+void wait()
+{
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return (1);
+	}
+	if (child_pid == 0)
+	{
+		printf("Wait for me, wait for me\n");
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+		printf("Oh, it's all better now\n");
+	}
+	return (0);
+}
