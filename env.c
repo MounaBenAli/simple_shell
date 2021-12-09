7524bae389e57b53cdecfa8d21563ed595ada4c5
MounaBenAli <benalimouna1986@gmail.com>
2021-12-09 04:16:53 -0800

    moyuna

--- env.c
+++ env.c
@@ -1,9 +1,8 @@
 #include"shell.h"
 
-/**
- * print_env: Implement the env built-in, that prints the current environment
- */
-
+ /**
+  *print_env - Implement the env built-in, that prints the current environment
+*/
 void print_env()
 {
   char **s = environ;
